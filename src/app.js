d5258cb51ac28aaea63bb9be69833e08c27b5079
Apurva Gaurav <app.gaurav19@gmail.com>
2026-03-22 23:44:54 +0530

    plyaing with routes

--- src/app.js
+++ src/app.js
@@ -6,10 +6,23 @@ app.listen(7777, () => {
   console.log("Server is running successfully on port 7777");
 });
 
-app.use("/test", (req, res) => {
-  res.send("Hello from test");
+// app.use("/user", (req, res) => {
+//   res.send("Hello from USER");
+// });
+
+//dynamic routes
+app.get("/user/:id/:name", (req, res) => {
+  // responding to get request
+  console.log(req.query);
+  res.send(`THIS IS USER with ${req.params.id} and name ${req.params.name} `);
 });
 
-app.use("/home", (req, res) => {
-  res.send("Hello from home page");
+app.post("/user", (req, res) => {
+  //saved data to database
+  res.send({ firstName: "Apurva", lastName: "Gaurav" });
+});
+
+app.delete("/user", (req, res) => {
+  //deleted data from database
+  res.send("Deleted data from DB successfully");
 });
